73302fd436366d753b7cd342538194c48dd0423d
Jordan <jordansampson23@gmail.com>
2023-04-07 17:24:35 -0500

    Imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-
+import {  Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import React, { useState } from "react";
 
 function App() {
+
+  const [authenticated, setAuthenticated] = useState(false)
+
+  const handleLoginSuccess = () => {
+    setAuthenticated(true);
+  }
+
+  const handleLogout = () => {
+    setAuthenticated(false);
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          {authenticated ? (
+            <Route path="/" element={<DashboardPage />} />
+          ) : (
+            <Route
+              path="/"
+              element={
+                <Navigate
+                  to="/login"
+                  replace
+                  state={{ from: window.location.pathname }}
+                />
+              }
+            />
+          )}
+        </Routes>
+      </div>
+    </>
   );
 }
 
