e84da3e30efea89fa4b04ed80f73810c5033d035
Jordan <jordansampson23@gmail.com>
2023-04-08 13:42:38 -0500

    showProfile state

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
+import auth from '../services/auth'
 
 const DashboardPage = ({ currentUser }) => {
+
+  const [showProfile, setShowProfile] = useState(false);
+
+  // Check if display name is null
+  if (currentUser.displayName === null) {
+    setShowProfile(true);
+  }
+
+
   return (
     <>
       <Header />
+      {/* if showProfile load profile component */}
     </>
   )
 }
