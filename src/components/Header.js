4157e6d3d7e0e8cbfba93ae2e7bbbe409e222fab
Jordan <jordansampson23@gmail.com>
2023-04-08 12:39:15 -0500

    Dashboard & Header  initiated

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+    background: linear-gradient(to right, #34e89e, #0f3443);
+    padding: .1em;
+`;
+
+const StyledLogo = styled.h1`
+    font-size = 2em;
+    color: #fff;
+    padding-left: .5em
+`
+
+const Header = () => {
+  return (
+    <StyledHeader>
+        <StyledLogo>Task</StyledLogo>
+    </StyledHeader>
+  )
+}
+
+export default Header
