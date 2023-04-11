168d37c0c7a07c9c239e8d3738e53827c466440f
Jordan <jordansampson23@gmail.com>
2023-04-11 10:45:21 -0500

    initialize styled page

---
+++ src/styles/StyledComponents.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Module = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1.5em;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    box-shadow: 0 15px 25px rgba(0,0,0,.6);
+    border-radius: 10px;
+    padding: 40px;
+`
+
+export const Title = styled.h1`
+  font-size: 1.5em;
+  color: #fff;
+  padding: .5em:
+`
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 10px 0;
+  font-size: 1.3em;
+  color: #fff;
+  margin-bottom: 30px;
+  border: none;
+  border-bottom: 1px solid #fff;
+  outline: none;
+  background: transparent;
+`
+
+export const Label = styled.label`
+  font-size: 1.3em;
+  padding: 10px 0;
+  color: #fff;
+  pointer-events: none;
+`
