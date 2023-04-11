4e8731f818de6948f44594830a545ea88cadd11c
Jordan <jordansampson23@gmail.com>
2023-04-11 13:56:40 -0500

    UpdatedUserProfile functional

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,43 +1,65 @@
 import React, { useState } from 'react'
-import { Module, Input, Label, Title } from '../../styles/StyledComponents'
+import { updateProfile } from 'firebase/auth';
+import { Module, Input, Label, Title, Button } from '../../styles/StyledComponents'
+import styled, { ThemeProvider } from 'styled-components';
+import theme from '../../styles/theme';
 
 
+const UserProfile = ({ authObject, setShowProfile }) => {
 
+    const [displayName, setDisplayName] = useState(authObject.currentUser.displayName || '');
+    const [email, setEmail] = useState(authObject.currentUser.email);
+    const [phoneNumber, setPhoneNumber] = useState(authObject.currentUser.phoneNumber || '');
 
-const UserProfile = ({ currentUser }) => {
+    const updateUserProfile = async () => {
+      try {
+        await updateProfile(authObject.currentUser, {
+          displayName,
+          email,
+          phoneNumber
+        });
+        console.log('Profile updated successfully!')
+      } catch (error) {
+        console.log('Error updating profile: ', error)
+      }
+    };
 
-    const [displayName, setDisplayName] = useState(null);
-    const [email, setEmail] = useState(currentUser.email);
-    const [phoneNumber, setPhoneNumber] = useState(currentUser.phoneNumber);
-
+    const handleCloseModule = () => {
+      setShowProfile(false);
+    };
 
   return (
-    <Module>
-      <Title>User Profile</Title>
-        <form>
-            <Label htmlFor='displayName' color='#fff' >Display Name:</Label>
-            <Input 
-            type="text"
-            id='displayName'
-            name='displayName'
-            onChange={(e) => setDisplayName(e.target.value)}
-            required />
-            <Label>Email:</Label>
-            <Input
-            type='email'
-            id='email'
-            name='email'
-            onChange={(e) => setEmail(e.target.value)}
-            value={currentUser.email} />
-            <Label>Phone Number:</Label>
-            <Input
-            type='tel'
-            id='phoneNumber'
-            name='phoneNumber'
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            value={currentUser.phoneNumber} />
-        </form>
-    </Module>
+    <ThemeProvider theme={theme} >
+      <Module>
+        <Title>User Profile</Title>
+          <form>
+              <Label htmlFor='displayName' color='#fff' >Display Name:</Label>
+              <Input
+              type="text"
+              id='displayName'
+              name='displayName'
+              onChange={(e) => setDisplayName(e.target.value)}
+              value={displayName}
+              required />
+              <Label>Email:</Label>
+              <Input
+              type='email'
+              id='email'
+              name='email'
+              onChange={(e) => setEmail(e.target.value)}
+              value={email} />
+              <Label>Phone Number:</Label>
+              <Input
+              type='tel'
+              id='phoneNumber'
+              name='phoneNumber'
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              value={phoneNumber} />
+              <Button primary type='button' onClick={updateUserProfile} >Save</Button>
+              <Button type='button' onClick={handleCloseModule} >Close</Button>
+          </form>
+      </Module>
+    </ThemeProvider>
   )
 }
 
