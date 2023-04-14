8fda909cb3e99559810c4679ab1940c94f6ccbeb
Jordan <jordansampson23@gmail.com>
2023-04-14 10:21:18 -0500

    getTasks fn & state added. Ability to retrieve tasks

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import Header from '../components/Header'
 import auth from '../services/auth'
+import db from '../services/storage'
+import { collection, getDocs, addDoc, collectionGroup, query, where } from 'firebase/firestore'
 import { onAuthStateChanged } from 'firebase/auth'
 import UserProfile from '../components/UserProfile/UserProfile'
 import { ThemeProvider } from 'styled-components'
@@ -9,9 +11,22 @@ import theme from '../styles/theme'
 const DashboardPage = () => {
 
   const [showProfile, setShowProfile] = useState(false);
-  const [authObject, setAuthObject] = useState(auth)
-  const [currentUser, setCurrentUser] = useState(null)
+  const [authObject, setAuthObject] = useState(auth);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
+  const usersCollectionRef = collection(db, "users")
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    }
+    getUsers()
+  }, []);
+
+  // Set the authObject & currentUser on authStateChange
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user)
@@ -20,11 +35,30 @@ const DashboardPage = () => {
     return unsubscribe;
   }, []);
 
+  // Set showProfile if user has never changed displayName
   useEffect(() => {
     if (currentUser && currentUser.displayName === null) {
       setShowProfile(true);
     }
-  }, [currentUser])
+  }, [currentUser]);
+
+  // Get user tasks
+  useEffect(() => {
+    const tasksQuery = currentUser ? query(
+      collectionGroup(db, 'tasks'),
+      where('userId', '==', currentUser.uid)
+    ) : null;
+
+    const getTasks = async () => {
+      if (tasksQuery) {
+        const data = await getDocs(tasksQuery);
+        console.log(data.docs)
+        setTasks(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+      }
+    }
+    getTasks();
+  }, [currentUser]);
+  
   
 
   return (
