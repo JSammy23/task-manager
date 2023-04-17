170a9ff9bfe2841bf7521276fe21d2e1836bd913
Jordan <jordansampson23@gmail.com>
2023-04-17 11:39:26 -0500

    Styling title, checkbox, note

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,20 +3,54 @@ import styled from 'styled-components'
 
 const Tile = styled.div`
     display: flex;
-    padding: .3em;
+    padding: .5em;
+    border-bottom: 1px solid #71717A;
+    align-items: center;
+`;
+
+const TaskInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: .2em;
 `
-const Title = styled.h2`
+
+const Title = styled.h3`
     color: #fff;
-`
+`;
+const Note = styled.p`
+    color: #D6D3D1;
+`;
+
+const Checkbox = styled.input`
+  appearance: none;
+  width: 20px;
+  height: 20px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  margin-right: 10px;
+
+  &:checked {
+    background-color: #34bc81;
+    border: 1px solid #000;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px #007aff33;
+  }
+`;
 
 const Task = ({ task }) => {
 
 
   return (
     <Tile>
-        <Title>{task.title}</Title>
-        {/* task.note */}
-        
+        <Checkbox type='checkbox' />
+        <TaskInfo>
+            <Title>{task.title}</Title>
+            <Note>{task.note}</Note>
+        </TaskInfo>
+
     </Tile>
   )
 }
