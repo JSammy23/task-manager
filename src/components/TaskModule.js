d58ee8e0b271e052285a5f2c4726c28de653fbd0
Jordan <jordansampson23@gmail.com>
2023-04-21 10:42:36 -0500

    Wrap module in form

--- src/components/TaskModule.js
+++ src/components/TaskModule.js
@@ -37,14 +37,16 @@ const TaskModule = ({header, action, task, showModule, addTask}) => {
     <ThemeProvider theme={theme}>
       <Module>
           <Title>{header}</Title>
-          <Label htmlFor='title'>Title: </Label>
-          <Input type='text' name='title' id='title' value={title} onChange={(e) => setTitle(e.target.value)} />
-          <Label htmlFor='note'>Note: </Label>
-          <Input type='text' name='note' id='note' value={note} onChange={(e) => setNote(e.target.value)} />
-          <Label htmlFor='dueDate'>dueDate: </Label>
-          <Input type='date' name='dueDate' id='dueDate' value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
-          <Button primary type='button' onClick={handleAddTask} >{action}</Button>
-          <Button type='button' onClick={handleCloseModule} >Cancel</Button>
+          <form>
+            <Label htmlFor='title'>Title: </Label>
+            <Input type='text' name='title' id='title' value={title} onChange={(e) => setTitle(e.target.value)} />
+            <Label htmlFor='note'>Note: </Label>
+            <Input type='text' name='note' id='note' value={note} onChange={(e) => setNote(e.target.value)} />
+            <Label htmlFor='dueDate'>dueDate: </Label>
+            <Input type='date' name='dueDate' id='dueDate' value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
+            <Button primary type='button' onClick={handleAddTask} >{action}</Button>
+            <Button type='button' onClick={handleCloseModule} >Cancel</Button>
+          </form>
       </Module>
     </ThemeProvider>
   )
